c73b894a6096d54c5cc9d7dff951b959c7b0630c
Mike Jovanovich <mpjovanovich@gmail.com>
2026-09-23 09:46:04 -0400

    Added a new course option to the random selections

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const messageElement = document.getElementById('message');
 // Array of friendly courseings
 const courses = [
     "INFM 109",
-    "SDEV 120"
+    "SDEV 120",
+    "SDEV 140"
 ];
 
 // Click event handler
